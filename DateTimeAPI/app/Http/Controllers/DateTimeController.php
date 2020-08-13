e531ae48d9f926fb0e8c71d00a3097e654dd614f
Wei <lvgoodgoodstudy@gmail.com>
2020-08-13 16:39:48 +0930

    Add function to convert result
    By adding third parameter - convert (seoncds, minutes, months,years) to transfer result into other units.

--- DateTimeAPI/app/Http/Controllers/DateTimeController.php
+++ DateTimeAPI/app/Http/Controllers/DateTimeController.php
@@ -47,29 +47,42 @@ class DateTimeController extends Controller
             $secondDateInCarbon = Carbon::createFromDate($secondDate);
         }
 
-        if(isset($request->convert)){
-            $convert = $request->convert;
-        }
-
         if($mode === '1'){
             // find out the number of days between two datetime
-            $days = $firstDateInCarbon->diffInDays($secondDateInCarbon, true);
+            $days = $firstDateInCarbon->diffInDays($secondDateInCarbon, true);//get number of days between two dates
             $result = 'About ' . $days . ($days > 1 ? ' days': ' day') . ' between these two dates';
-            return response()->json($result);
         } else if ($mode === '2') {
             //find out the number of weekdays between two datetime
-            $days = $firstDateInCarbon->diffInWeekdays($secondDateInCarbon, true);
+            $days = $firstDateInCarbon->diffInWeekdays($secondDateInCarbon, true);//get number of weekdays between two dates
             $result = 'About ' . $days . ($days > 1 ? ' weekdays': ' weekday') . ' between these two dates';
-            return response()->json($result);
         } else if ($mode === '3') {
             //find out the number of complete weeks between two datetime
-            $weeks = $firstDateInCarbon->diffInWeeks($secondDateInCarbon, true);
+            $weeks = $firstDateInCarbon->diffInWeeks($secondDateInCarbon, true);//get number of weeks between two dates
             $result = 'About ' . $weeks . ($weeks > 1 ? ' complete weeks': ' complete week') . ' between these two dates';
-            return response()->json($result);
         } else if ($mode === '4') {
             //Allow the specification of a timezone for comparison of input parameters from
             //different timezones.
 
         }
+
+        if(isset($request->convert) && $mode != '4'){// check whether needs to transfer result
+            $convert = $request->convert;
+            if($convert == 'seconds'){
+                $gaps =  $firstDateInCarbon->diffInSeconds($secondDateInCarbon, true);
+                $result = 'About ' . $gaps . ($gaps > 1 ? ' seconds': ' second') . ' between these two dates';
+            } else if($convert == 'minutes') {
+                $gaps =  $firstDateInCarbon->diffInMinutes($secondDateInCarbon, true);
+                $result = 'About ' . $gaps . ($gaps > 1 ? ' minutes': ' minute') . ' between these two dates';
+            } else if ($convert == 'hours') {
+                $gaps =  $firstDateInCarbon->diffInMonths($secondDateInCarbon, true);
+                $result = 'About ' . $gaps . ($gaps > 1 ? ' hours': ' hour') . ' between these two dates';
+            } else if ($convert == 'years') {
+                $gaps =  $firstDateInCarbon->diffInYears($secondDateInCarbon, true);
+                $result = 'About ' . $gaps . ($gaps > 1 ? ' years': ' year') . ' between these two dates';
+            } else {
+                $result = 'Convert type is not correct. Please choose from "second, minutes, hours, years"';
+            }
+        }
+        return response()->json($result);
     }
 }
