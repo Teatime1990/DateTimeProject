fbe3a3a1d99b4f846d41a0dd5854748781fb1e11
Wei <lvgoodgoodstudy@gmail.com>
2020-08-13 17:02:54 +0930

    Add unit tests for finished functions
    Unit test debugging

--- DateTimeAPI/tests/DateTimeUnitTest.php
+++ DateTimeAPI/tests/DateTimeUnitTest.php
@@ -4,7 +4,6 @@ use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 class BasicTest extends TestCase
 {
-
     public function testExample()
     {
         $this->assertTrue(true);
@@ -15,8 +14,63 @@ class BasicTest extends TestCase
         $response->seeStatusCode(200);
     }
 
-    public function test_correct_days_between_two_datetime(){
-        $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-14 12:22:33&mode=1");
-        $response->assertEquals('"About 2 days between these two dates"',$this->response->getContent());
+    public function test_incorrect_inputs(){
+        $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-104 12:22:33");
+        $response->assertEquals('"Second Date is not valid"',$this->response->getContent());
+    }
+
+    public function test_correct_days_between_two_datetime1(){
+        $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-14 12:22:33");
+        $response->assertEquals('"About 1 days between these two dates"',$this->response->getContent());
+    }
+
+    public function test_correct_days_between_two_datetime2(){
+        $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-24 12:22:33");
+        $response->assertEquals('"About 11 days between these two dates"',$this->response->getContent());
+    }
+
+    public function test_correct_weekdays_between_two_datetime1(){
+        $response = $this->get("/api/DateTimeAPI/2?firstDate=2020-08-13 00:00:22&secondDate=2020-09-14 12:22:33");
+        $response->assertEquals('"About 23 weekdays between these two dates"',$this->response->getContent());
+    }
+
+    public function test_correct_weekdays_between_two_datetime2(){
+        $response = $this->get("/api/DateTimeAPI/2?firstDate=2020-08-13 00:00:22&secondDate=2020-10-15 12:22:33");
+        $response->assertEquals('"About 46 weekdays between these two dates"',$this->response->getContent());
+    }
+
+    public function test_correct_weeks_between_two_datetime1(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-08-28 12:22:33");
+        $response->assertEquals('"About 2 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_correct_weeks_between_two_datetime2(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_incorrect_convert_input(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=second");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_convert_to_seconds(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=seconds");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_convert_to_minutes(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=minutes");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_convert_to_hours(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=hours");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+    }
+
+    public function test_convert_to_years(){
+        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=years");
+        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
     }
 }
