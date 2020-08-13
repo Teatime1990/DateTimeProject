3b17ca69bf955fa948c6826bda4ac64da9c362e2
Wei <lvgoodgoodstudy@gmail.com>
2020-08-13 21:43:32 +0930

    Complete convert function & update Unit Tests
    Convert function for mode 1 , 2 ,3 has been updated
    Unit tests have been updated

--- DateTimeAPI/app/Http/Controllers/DateTimeController.php
+++ DateTimeAPI/app/Http/Controllers/DateTimeController.php
@@ -7,18 +7,9 @@ use Carbon\Carbon;
 
 class DateTimeController extends Controller
 {
-    /**
-     * DatetimeController
-     */
-    public function __construct()
-    {
-        //
-    }
-
     public function handleData(Request $request, $mode) { //main function to deal with data from client side
         if((!isset($request->firstDate) || !isset($request->secondDate)) && !isset($request->timezone)){
-            $message = 'Lack of parameter';
-            return response()->json($message);
+            return response()->json('Lack of parameter.');
         } //validation
 
         if(isset($request->firstDate)){
@@ -26,7 +17,7 @@ class DateTimeController extends Controller
             if (date('Y-m-d H:i:s', strtotime($firstDate)) == $firstDate) {
                 $firstDateInCarbon = Carbon::createFromDate($firstDate);
             } else {
-                return response()->json('First Date is not valid');
+                return response()->json('First Date is not valid.');
             }
         }
 
@@ -35,46 +26,88 @@ class DateTimeController extends Controller
             if (date('Y-m-d H:i:s', strtotime($secondDate)) == $secondDate) {
                 $secondDateInCarbon = Carbon::createFromDate($secondDate);
             } else {
-                return response()->json('Second Date is not valid');
+                return response()->json('Second Date is not valid.');
             }
         }
 
         if($mode === '1'){
             // find out the number of days between two datetime
             $days = $firstDateInCarbon->diffInDays($secondDateInCarbon, true);//get number of days between two dates
-            $result = 'About ' . $days . ($days > 1 ? ' days': ' day') . ' between these two dates';
+            $result = 'About ' . $days . ($days > 1 ? ' days': ' day') . ' between these two dates.';
+
+            //if the result needs to be transfered, third parameter accepted
+            if(isset($request->convert)){
+                $convert = $request->convert;
+                if($convert == 'seconds'){
+                    $second = $days * 24 * 60 * 60;
+                    $result .= " In terms of second, it is " . $second . ($second > 1 ? ' seconds.': ' second.');
+                } else if($convert == 'minutes') {
+                    $minute = $days * 24 * 60;
+                    $result .= " In terms of minute, it is " . $minute . ($minute > 1 ? ' minutes.': ' minute.');
+                } else if ($convert == 'hours') {
+                    $hour = $days * 24;
+                    $result .= " In terms of hour, it is " . $hour . ($hour > 1 ? ' hours.': ' hour.');
+                } else if ($convert == 'years') {
+                    $year = $days / 365;
+                    $result .= " In terms of year, it is " . $year . ($year > 1 ? ' years.': ' year.');
+                } else {
+                    $result = 'Convert type is not correct. Please choose from second, minutes, hours, years.';
+                }
+            }
         } else if ($mode === '2') {
             //find out the number of weekdays between two datetime
             $days = $firstDateInCarbon->diffInWeekdays($secondDateInCarbon, true);//get number of weekdays between two dates
-            $result = 'About ' . $days . ($days > 1 ? ' weekdays': ' weekday') . ' between these two dates';
+            $result = 'About ' . $days . ($days > 1 ? ' weekdays': ' weekday') . ' between these two dates.';
+
+            //if the result needs to be transfered, third parameter accepted
+            if(isset($request->convert)){
+                $convert = $request->convert;
+                if($convert == 'seconds'){
+                    $second = $days * 24 * 60 * 60;
+                    $result .= " In terms of second, it is " . $second . ($second > 1 ? ' seconds.': ' second.');
+                } else if($convert == 'minutes') {
+                    $minute = $days * 24 * 60;
+                    $result .= " In terms of minute, it is " . $minute . ($minute > 1 ? ' minutes.': ' minute.');
+                } else if ($convert == 'hours') {
+                    $hour = $days * 24;
+                    $result .= " In terms of hour, it is " . $hour . ($hour > 1 ? ' hours.': ' hour.');
+                } else if ($convert == 'years') {
+                    $year = $days / 365;
+                    $result .= " In terms of year, it is " . $year . ($year > 1 ? ' years.': ' year.');
+                } else {
+                    $result = 'Convert type is not correct. Please choose from second, minutes, hours, years.';
+                }
+            }
         } else if ($mode === '3') {
             //find out the number of complete weeks between two datetime
             $weeks = $firstDateInCarbon->diffInWeeks($secondDateInCarbon, true);//get number of weeks between two dates
-            $result = 'About ' . $weeks . ($weeks > 1 ? ' complete weeks': ' complete week') . ' between these two dates';
+            $result = 'About ' . $weeks . ($weeks > 1 ? ' complete weeks': ' complete week') . ' between these two dates.';
+
+            //if the result needs to be transfered, third parameter accepted
+            if(isset($request->convert)){
+                $convert = $request->convert;
+                if($convert == 'seconds'){
+                    $second = $weeks * 7 * 24 * 60 * 60;
+                    $result .= " In terms of second, it is " . $second . ($second > 1 ? ' seconds.': ' second.');
+                } else if($convert == 'minutes') {
+                    $minute = $weeks * 7 * 24 * 60;
+                    $result .= " In terms of minute, it is " . $minute . ($minute > 1 ? ' minutes.': ' minute.');
+                } else if ($convert == 'hours') {
+                    $hour = $weeks * 7 * 24;
+                    $result .= " In terms of hour, it is " . $hour . ($hour > 1 ? ' hours.': ' hour.');
+                } else if ($convert == 'years') {
+                    $year = round($weeks * 7 / 365, 2);
+                    $result .= " In terms of year, it is " . $year . ($year > 1 ? ' years.': ' year.');
+                } else {
+                    $result = 'Convert type is not correct. Please choose from second, minutes, hours, years.';
+                }
+            }
         } else if ($mode === '4') {
             //Allow the specification of a timezone for comparison of input parameters from
             //different timezones.
 
         }
 
-        if(isset($request->convert) && $mode != '4'){// check whether needs to transfer result
-            $convert = $request->convert;
-            if($convert == 'seconds'){
-                $gaps =  $firstDateInCarbon->diffInSeconds($secondDateInCarbon, true);
-                $result = 'About ' . $gaps . ($gaps > 1 ? ' seconds': ' second') . ' between these two dates';
-            } else if($convert == 'minutes') {
-                $gaps =  $firstDateInCarbon->diffInMinutes($secondDateInCarbon, true);
-                $result = 'About ' . $gaps . ($gaps > 1 ? ' minutes': ' minute') . ' between these two dates';
-            } else if ($convert == 'hours') {
-                $gaps =  $firstDateInCarbon->diffInHours($secondDateInCarbon, true);
-                $result = 'About ' . $gaps . ($gaps > 1 ? ' hours': ' hour') . ' between these two dates';
-            } else if ($convert == 'years') {
-                $gaps =  $firstDateInCarbon->diffInYears($secondDateInCarbon, true);
-                $result = 'About ' . $gaps . ($gaps > 1 ? ' years': ' year') . ' between these two dates';
-            } else {
-                $result = 'Convert type is not correct. Please choose from second, minutes, hours, years';
-            }
-        }
         return response()->json($result);
     }
 }
--- DateTimeAPI/tests/DateTimeUnitTest.php
+++ DateTimeAPI/tests/DateTimeUnitTest.php
@@ -17,61 +17,61 @@ class BasicTest extends TestCase
 
     public function test_incorrect_inputs(){
         $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-104 12:22:33");
-        $response->assertEquals('"Second Date is not valid"',$this->response->getContent());
+        $response->assertEquals('"Second Date is not valid."',$this->response->getContent());
     }
 
     public function test_correct_days_between_two_datetime1(){
         $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-14 12:22:33");
-        $response->assertEquals('"About 1 day between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 1 day between these two dates."',$this->response->getContent());
     }
 
     public function test_correct_days_between_two_datetime2(){
         $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-08-24 12:22:33");
-        $response->assertEquals('"About 11 days between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 11 days between these two dates."',$this->response->getContent());
     }
 
     public function test_correct_weekdays_between_two_datetime1(){
         $response = $this->get("/api/DateTimeAPI/2?firstDate=2020-08-13 00:00:22&secondDate=2020-09-14 12:22:33");
-        $response->assertEquals('"About 23 weekdays between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 23 weekdays between these two dates."',$this->response->getContent());
     }
 
     public function test_correct_weekdays_between_two_datetime2(){
         $response = $this->get("/api/DateTimeAPI/2?firstDate=2020-08-13 00:00:22&secondDate=2020-10-15 12:22:33");
-        $response->assertEquals('"About 46 weekdays between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 46 weekdays between these two dates."',$this->response->getContent());
     }
 
     public function test_correct_weeks_between_two_datetime1(){
         $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-08-28 12:22:33");
-        $response->assertEquals('"About 2 complete weeks between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 2 complete weeks between these two dates."',$this->response->getContent());
     }
 
     public function test_correct_weeks_between_two_datetime2(){
         $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33");
-        $response->assertEquals('"About 21 complete weeks between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 21 complete weeks between these two dates."',$this->response->getContent());
     }
 
     public function test_incorrect_convert_input(){
         $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=second");
-        $response->assertEquals('"Convert type is not correct. Please choose from second, minutes, hours, years"',$this->response->getContent());
+        $response->assertEquals('"Convert type is not correct. Please choose from second, minutes, hours, years."',$this->response->getContent());
     }
 
     public function test_convert_to_seconds(){
         $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-03-14 12:22:33&convert=seconds");
-        $response->assertEquals('"About 13088269 seconds between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 21 complete weeks between these two dates. In terms of second, it is 12700800 seconds."',$this->response->getContent());
     }
 
     public function test_convert_to_minutes(){
-        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-09-14 12:22:33&convert=minutes");
-        $response->assertEquals('"About 46822 minutes between these two dates"',$this->response->getContent());
+        $response = $this->get("/api/DateTimeAPI/2?firstDate=2020-08-13 00:00:22&secondDate=2020-09-14 12:22:33&convert=minutes");
+        $response->assertEquals('"About 23 weekdays between these two dates. In terms of minute, it is 33120 minutes."',$this->response->getContent());
     }
 
     public function test_convert_to_hours(){
-        $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2020-10-04 12:42:33&convert=hours");
-        $response->assertEquals('"About 1260 hours between these two dates"',$this->response->getContent());
+        $response = $this->get("/api/DateTimeAPI/1?firstDate=2020-08-13 00:00:22&secondDate=2020-10-04 12:42:33&convert=hours");
+        $response->assertEquals('"About 52 days between these two dates. In terms of hour, it is 1248 hours."',$this->response->getContent());
     }
 
     public function test_convert_to_years(){
         $response = $this->get("/api/DateTimeAPI/3?firstDate=2020-08-13 00:00:22&secondDate=2025-03-14 12:22:33&convert=years");
-        $response->assertEquals('"About 4 years between these two dates"',$this->response->getContent());
+        $response->assertEquals('"About 239 complete weeks between these two dates. In terms of year, it is 4.58 years."',$this->response->getContent());
     }
 }
